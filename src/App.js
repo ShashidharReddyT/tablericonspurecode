67a46c10afb9ee0d6dcaa9be56cd47b43bfc0764
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-02-15 18:17:16 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
 
+import Tablericons from "./Components/Tablericons.js";
+// defaultTheme
+import themes from "./themes";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes("customization")}>
+        <CssBaseline />
+        <Tablericons />
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
